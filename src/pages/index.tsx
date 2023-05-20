bac252ba722a235b3906a6036136ad3c36f40143
Suraj <suraj.chand@vedastudios.com>
2023-05-20 11:19:37 +0545

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,136 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useCallback, useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+export const dataImage = [
+  {
+    title: "0",
+    imageSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2eA4njppJycpcrF5mqpynKLo8nZCQDVGjv5gUZH8-BI0R6vELBOoDB5fcjbutMmmA7Y4&usqp=CAU",
+  },
+  {
+    title: "1",
+    imageSrc:
+      "https://gamevault999.com/wp-content/uploads/2022/04/lucky777-neon.png",
+  },
+  {
+    title: "2",
+    imageSrc:
+      "https://gamevault999.com/wp-content/uploads/2022/04/fruitslot-neon.png",
+  },
+  {
+    title: "3",
+    imageSrc:
+      "https://gamevault999.com/wp-content/uploads/2022/04/oceanking3-neon.png",
+  },
+  {
+    title: "4",
+    imageSrc:
+      "https://gamevault999.com/wp-content/uploads/2022/04/fruitslot-neon.png",
+  },
+  {
+    title: "5",
+    imageSrc:
+      "https://gamevault999.com/wp-content/uploads/2022/04/PLINKO2-1.png",
+  },
+  {
+    title: "6",
+    imageSrc:
+      "https://gamevault999.com/wp-content/uploads/2022/04/lucky777-neon.png",
+  },
+  {
+    title: "7",
+    imageSrc:
+      "https://gamevault999.com/wp-content/uploads/2022/04/PLINKO2-1.png",
+  },
+  {
+    title: "8",
+    imageSrc:
+      "https://gamevault999.com/wp-content/uploads/2022/04/oceanking3-neon.png",
+  },
+  {
+    title: "9",
+    imageSrc:
+      "https://gamevault999.com/wp-content/uploads/2022/04/fruitslot-neon.png",
+  },
+];
 
-export default function Home() {
+const CarouselSlide1Item = () => {
+  const [dataToShow, setMyData] = useState(dataImage);
+
+  function rollUpArray(array: any) {
+    const rolledArray = [];
+    for (let i = 0; i < array.length; i++) {
+      const rolledIndex = (i + 1) % array.length;
+      rolledArray[rolledIndex] = array[i];
+    }
+
+    setMyData(rolledArray);
+  }
+
+  const continuousFunction = useCallback(() => {
+    rollUpArray(dataToShow);
+  }, [dataToShow]);
+
+  useEffect(() => {
+    const intervalId = setInterval(continuousFunction, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [continuousFunction]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex items-end p-10 my-20 bg-gray-700 border-4 border-green-400 w-fit">
+      {dataToShow.slice(0, 5).map((data, index) => {
+        let width = 180;
+        let height = 270;
+
+        if (index === 1) {
+          width = 200;
+          height = 300;
+        }
+        if (index === 2) {
+          width = 220;
+          height = 330;
+        }
+        if (index === 3) {
+          width = 240;
+          height = 360;
+        }
+        if (index === 4) {
+          width = 280;
+          height = 390;
+        }
+        // let zInex;
+        // if (index === 1 || index === 2 || index === 3) {
+        //   zInex = "!z-50";
+        // }
+        return (
+          <div
+            key={data.title}
+            style={{
+              height: height,
+              width: width,
+              // border: "2px solid red",
+            }}
+            className={`ml-10 text-[30px] ${
+              index === 1 || index === 2 || (index === 3 && "!z-50")
+            } object-fill bg-cover bg-no-repeat  w-full`}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <picture>
+              <img
+                src={data.imageSrc}
+                alt={data.title}
+                className={`w-full h-full ${
+                  dataToShow.length >= 5 && `image_${index}`
+                } `}
+              />
+            </picture>
+            {data.title}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default CarouselSlide1Item;
